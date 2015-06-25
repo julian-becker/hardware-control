ff7d7dd2390fd0c206c59b073ac3b525382d3df8
Julian Becker <becker.julian@gmail.com>
2015-06-25 06:54:23 +0200

    Probably, the IVisitorTyped is not needed at all. So I removed it.

--- hardware-control/Framework/Interfaces/IVisitor.h
+++ hardware-control/Framework/Interfaces/IVisitor.h
@@ -14,27 +14,3 @@ IVisitor<VISITABLE> {
     virtual Any visit(VISITABLE&) = 0;
 };
 
-
-template <typename R, typename INTERFACE, typename VISITABLE, typename...VISITABLES> struct
-IVisitorTypedImpl : IVisitorTypedImpl<R,INTERFACE,VISITABLES...> {
-    using visit_return_type = R;
-    using IVisitorTypedImpl<R,INTERFACE,VISITABLES...>::visit;
-    using IVisitorTypedImpl<R,INTERFACE,VISITABLES...>::visit_typed;
-    
-    virtual R visit_typed(VISITABLE& visitable) {
-        return visit(visitable).template as<R>();
-    }
-};
-
-template <typename R, typename INTERFACE, typename VISITABLE> struct
-IVisitorTypedImpl<R,INTERFACE,VISITABLE> : INTERFACE {
-    using INTERFACE::visit;
-    using visit_return_type = R;
-    
-    virtual R visit_typed(VISITABLE& visitable) {
-        return visit(visitable).Any::template as<R>();
-    }
-};
-
-template <typename R, typename...VISITABLES> using
-IVisitorTyped = IVisitorTypedImpl<R, IVisitor<VISITABLES...>, VISITABLES...>;
